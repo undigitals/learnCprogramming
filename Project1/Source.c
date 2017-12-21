1f0de5030be1dcb8285536c3397478f472c45df0
thevisionist <undigitals@gmail.com>
2017-12-21 10:33:06 +0900

    1.variables

---
+++ Project1/Source.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	double pettycash;
+	double grandtotal;
+
+	grandtotal = 500.50;
+	pettycash = 10.5;
+	printf("pettycash=%.3f\n", pettycash);
+	printf("grandtotal=%.2f\n", grandtotal);
+	return 0;
+
+}
