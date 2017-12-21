a8f29f5de0d857f09bfbae60aaec746a003364f5
thevisionist <undigitals@gmail.com>
2017-12-21 11:31:38 +0900

    5.Real constant

--- Project1/Source.c
+++ Project1/Source.c
@@ -2,12 +2,10 @@
 #include <corecrt_math.h>
 
 int main() {
-#define PI 3.141593 // this constant type variable declaring it directly instruction to preprocessor
-	                // NO Equals, NO semi-colon;
+	const double  PI = 3.141593;  // declaring const with const key makes real constant we 
+								  // we cannot redefine it
+	printf("%.6f\n", PI);
 
-	printf("%.6f\n", PI);
-#define PI 5.00		//so here we can it is not really constant as we can redefine it
-	printf("%.6f\n", PI);
 	return 0;
 
 }
