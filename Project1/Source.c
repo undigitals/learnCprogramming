0223bf22401e6d5d99038f3b8e2407924ac23540
thevisionist <undigitals@gmail.com>
2017-12-21 11:27:48 +0900

    4.Constant (2)

--- Project1/Source.c
+++ Project1/Source.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <corecrt_math.h>
 
 int main() {
 #define PI 3.141593 // this constant type variable declaring it directly instruction to preprocessor
 	                // NO Equals, NO semi-colon;
 
-	printf("%\n", PI);
+	printf("%.6f\n", PI);
+#define PI 5.00		//so here we can it is not really constant as we can redefine it
+	printf("%.6f\n", PI);
 	return 0;
 
 }
